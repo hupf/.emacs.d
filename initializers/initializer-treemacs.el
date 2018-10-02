b8f50e36c6fdc6b21ed07f88d98316c3be379ecc
Mathis Hofer <hofer@puzzle.ch>
2018-10-02 22:03:45 +0200

    Adjust treemacs config for newer version

--- initializers/initializer-treemacs.el
+++ initializers/initializer-treemacs.el
@@ -18,23 +18,24 @@
   )
 
   :custom-face
-  (treemacs-directory-face ((t (:inherit font-lock-type-face :height 90))))
-  (treemacs-header-face ((t (:inherit font-lock-comment-face))))
-  (treemacs-file-face ((t (:inherit default :height 90))))
-  (treemacs-term-node-face ((t (:inherit font-lock-string-face :height 90))))
-  (treemacs-git-modified-face ((t (:inherit font-lock-variable-name-face :height 90))))
-  (treemacs-git-renamed-face ((t (:inherit font-lock-doc-face :height 90))))
-  (treemacs-git-ignored-face ((t (:inherit font-lock-comment-face :height 90))))
-  (treemacs-git-untracked-face ((t (:inherit font-lock-string-face :height 90))))
-  (treemacs-git-added-face ((t (:inherit font-lock-type-face :height 90))))
-  (treemacs-git-conflict-face ((t (:inherit error :height 90))))
-  (treemacs-tags-face ((t (:inherit font-lock-builtin-fac :height 90))))
+  ;; TODO: bigger font for mac, smaller for linux?
+  ;; (treemacs-directory-face ((t (:inherit font-lock-type-face :height 90))))
+  ;; (treemacs-header-face ((t (:inherit font-lock-comment-face))))
+  ;; (treemacs-file-face ((t (:inherit default :height 90))))
+  ;; (treemacs-term-node-face ((t (:inherit font-lock-string-face :height 90))))
+  ;; (treemacs-git-modified-face ((t (:inherit font-lock-variable-name-face :height 90))))
+  ;; (treemacs-git-renamed-face ((t (:inherit font-lock-doc-face :height 90))))
+  ;; (treemacs-git-ignored-face ((t (:inherit font-lock-comment-face :height 90))))
+  ;; (treemacs-git-untracked-face ((t (:inherit font-lock-string-face :height 90))))
+  ;; (treemacs-git-added-face ((t (:inherit font-lock-type-face :height 90))))
+  ;; (treemacs-git-conflict-face ((t (:inherit error :height 90))))
+  ;; (treemacs-tags-face ((t (:inherit font-lock-builtin-fac :height 90))))
 
   :bind
-  ("C-S-b" . treemacs-toggle-auto-projectile)
+  ("C-S-b" . treemacs)
   (:map treemacs-mode-map
     ;; Mouse click on file opens it
-    ;; ("<mouse-1>" . treemacs-visit-node-no-split)
+    ("<mouse-1>" . treemacs-single-click-expand-action)
 
     ;; Open with OS native browser/application
     ("C-<return>" . treemacs-natively-open-path-at-point))
@@ -42,12 +43,6 @@
   :config
   (treemacs-filewatch-mode)
   (treemacs-git-mode 'simple)
-
-  ;; Activate treemacs-projectile if switching to project and treemacs is open
-  (add-hook 'projectile-after-switch-project-hook
-    '(lambda ()
-       (if (eq (treemacs--current-visibility) 'visible)
-           (treemacs-projectile))))
 )
 
 (use-package treemacs-projectile
@@ -55,16 +50,11 @@
   :defer t
 )
 
-(defun treemacs-toggle-auto-projectile ()
-  "Toggle treemacs considering whether in projectile project"
-  (interactive)
-  (if (eq (projectile-project-p) nil) (treemacs-toggle) (treemacs-projectile-toggle)))
-
 (defun treemacs-natively-open-path-at-point ()
   "Open path at point with native browser/application"
   (interactive)
-  ;; TODO: fix, treemacs--prop-at-point returns nil
+  ;; TODO: use "gio open" instead of "gnome-open"
   (let ((command (if (eq system-type 'darwin) "open" "gnome-open")))
-       (call-process-shell-command command nil nil nil (treemacs--prop-at-point 'abs-path))))
+       (call-process-shell-command command nil nil nil (treemacs--prop-at-point :path))))
 
 (provide 'initializer-treemacs)
