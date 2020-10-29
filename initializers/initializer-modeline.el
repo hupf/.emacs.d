3e322a3e25f38f3c60050d1d983e992111bd3cc8
Mathis Hofer <mathis@fsfe.org>
2020-10-29 20:46:05 +0100

    Disable audible bell

--- initializers/initializer-modeline.el
+++ initializers/initializer-modeline.el
@@ -1,7 +1,18 @@
-;; TODO
-;; ;;; initializer-modeline.el --- Setup our modeline.
+;;; initializer-lsp --- Setup modeline
+;;;
+;;; Commentary:
+;;;
+;;; Setup of the modeline look and behavior
+;;;
+;;; Code:
 
+;; Display column number in modeline
 (column-number-mode t)
+
+;; Replace the annoying bell ring with modeline flash
+(use-package mode-line-bell
+  :ensure t
+  :config (mode-line-bell-mode))
 
 (provide 'initializer-modeline)
 ;;; initializer-modeline.el ends here
