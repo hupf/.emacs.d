44f267970e9ba3fcaba93eade6e8c3d3751f6d45
Mathis Hofer <hofer@puzzle.ch>
2020-12-10 16:46:32 +0100

    Improve completion documentation

--- initializers/initializer-completion.el
+++ initializers/initializer-completion.el
@@ -1,30 +1,26 @@
-;; -- Ivy setup --
-;; https://github.com/abo-abo/swiper
-;; Ivy has a lot of different moving parts which can be unecessarily
-;; confusing. There is (i think) Ivy, the completion system which is
-;; just the low-level set of apis for turning text into a set of
-;; matches. Then there is Swiper, the UI that interacts with matching
-;; in the mini-buffer. And then finally, Counsel, which provides and
-;; extends a bunch of core emacs commands. So Ivy, Swiper and Counsel,
-;; are all packages that are part of the same system, so we'll refer
-;; to them as Ivy from here on out, even though it might technically
-;; refer to a sub-part.
+;;; initializer-completion --- Setup completion
+;;;
+;;; Commentary:
+;;;
+;;; Setup of mini buffer and text completion
+;;;
+;;; Code:
 
 ;; Minibuffer completion (ivy/ido alternative)
 (use-package selectrum
   :ensure t
   :config (selectrum-mode +1))
 
-;; Better selectrum sorting/filtering
+;; Improved selectrum sorting/filtering
 (use-package selectrum-prescient
   :after selectrum
   :defer 1
 
   :config
-  ;; to make sorting and filtering more intelligent
+  ;; To make sorting and filtering more intelligent
   (selectrum-prescient-mode +1)
 
-  ;; to save your command history on disk, so the sorting gets more
+  ;; To save command history on disk, so the sorting gets more
   ;; intelligent over time
   (prescient-persist-mode +1))
 
@@ -50,7 +46,7 @@
    (setq company-tooltip-align-annotations t)
 
    :config
-   (global-company-mode 1))
+   (global-company-mode +1))
 
 ;; Fancy company dialog with icons
 (use-package company-box
@@ -59,3 +55,4 @@
   :hook (company-mode . company-box-mode))
 
 (provide 'initializer-completion)
+;;; initializer-completion.el ends here
