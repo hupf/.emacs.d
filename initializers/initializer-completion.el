20bc473265dfc8740e2d63e7c0d8e6a266d30bbf
Mathis Hofer <hofer@puzzle.ch>
2021-02-11 14:41:59 +0100

    Add ctrlf minibuffer focusing, but disable ctrlf for now

--- initializers/initializer-completion.el
+++ initializers/initializer-completion.el
@@ -27,20 +27,40 @@
   ;; intelligent over time
   (prescient-persist-mode t))
 
-;; Improved C-s search (swiper alternative)
+;; Improved C-s search (isearch/swiper alternative)
 (use-package ctrlf
+  :disabled
   :defer 0
+
   :custom
   ;; Don't show match count within content on current line
   (ctrlf-show-match-count-at-eol nil)
 
   :config
+  ;; Wrap all functions bound to keys
+  (advice-add 'ctrlf-forward-literal :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-backward-literal :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-forward-regexp :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-backward-regexp :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-forward-symbol :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-forward-symbol-at-point :around #'ctrlf-with-minibuffer-focus)
+  (advice-add 'ctrlf-backward-literal :around #'ctrlf-with-minibuffer-focus)
+
   ;; kill-whole-line should kill line in buffer, not minibuffer
   (add-to-list
    'ctrlf-minibuffer-bindings
    '("<C-S-backspace>" . ctrlf-exit-and-kill-whole-line) t)
 
   (ctrlf-mode t))
+
+(defun ctrlf-with-minibuffer-focus (orig-fun &rest args)
+  "Wrap the given function ORIG-FUN with ARGS and focus minibuffer if search is active but cursor in buffer."
+  (interactive)
+  (if (and ctrlf--active-p (not (minibufferp (current-buffer))))
+      ;; In buffer but with active search, so only focus minibuffer
+      (select-window (active-minibuffer-window))
+    ;; Original behavior (call wrapped ctrlf function)
+    (apply orig-fun args)))
 
 (defun ctrlf-exit-and-kill-whole-line ()
   "Kill whole line of buffer after exiting ctrlf session."
