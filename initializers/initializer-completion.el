1fe132aeac4daff4b3ac316f6c63c14564cad667
Mathis Hofer <hofer@puzzle.ch>
2022-02-25 09:56:39 +0100

    Use replace selectrum with vertico & add marginalia

--- initializers/initializer-completion.el
+++ initializers/initializer-completion.el
@@ -7,25 +7,76 @@
 ;;; Code:
 
 ;; Minibuffer completion (ivy/ido alternative)
-(use-package selectrum
+(use-package vertico
   :defer 0
-  :config (selectrum-mode t))
+  :init
+  (vertico-mode)
 
-;; Improved selectrum sorting/filtering
-(use-package selectrum-prescient
-  :after selectrum
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
 
-  :custom
-  ;; Enable fuzzy matching as well
-  (prescient-filter-method '(literal regexp initialism fuzzy))
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
 
-  :config
-  ;; To make sorting and filtering more intelligent
-  (selectrum-prescient-mode t)
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
 
-  ;; To save command history on disk, so the sorting gets more
-  ;; intelligent over time
-  (prescient-persist-mode t))
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Optionally use the `orderless' completion style. See
+;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
+;; dispatcher. Additionally enable `partial-completion' for file path
+;; expansion. `partial-completion' is important for wildcard support.
+;; Multiple files can be opened at once with `find-file' if you enter a
+;; wildcard. You may also give the `initials' completion style a try.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; Alternatively try `consult-completing-read-multiple'.
+  (defun crm-indicator (args)
+    (cons (concat "[CRM] " (car args)) (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+
+;; Enable richer minibuffer annotations
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
 
 ;; Improved C-s search (isearch/swiper alternative)
 (use-package ctrlf
