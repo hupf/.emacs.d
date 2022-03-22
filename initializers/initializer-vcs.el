a749826b87185b5be19f09a8117d8fff4d642934
Mathis Hofer <hofer@puzzle.ch>
2022-03-22 09:37:37 +0100

    Setup Node for Magit to make Git hook commands working

--- initializers/initializer-vcs.el
+++ initializers/initializer-vcs.el
@@ -1,12 +1,17 @@
 (use-package magit
   :defer 0
 
-;; TODO: needed?
-;; (global-magit-file-mode 1)
-
   :bind
-  (("C-c g" . magit-status)
-   ("C-c M-g" . magit-dispatch-popup)))
+  (("C-c g" . magit-status-with-node)
+   ("C-c M-g" . magit-dispatch)))
+
+(defun magit-status-with-node ()
+  "Configure project's Node.js and node_modules for Git hook commands to be working in Magit"
+  (interactive)
+  (nvm-use-for (magit-toplevel))
+  (add-node-modules-path)
+  (magit-status-setup-buffer))
+
 
 ;; use diff-hl to add git diffs in gutter
 (use-package diff-hl
